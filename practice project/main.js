9c59410f9adf82af995128e8d2d802cb0d622eef
Archit Adhikari <33290440+ArchitAdh@users.noreply.github.com>
2020-05-26 23:42:34 +0545

    added search items functionality

--- practice project/main.js
+++ practice project/main.js
@@ -1,6 +1,7 @@
 //DECLARATIONS
 var form = document.getElementById('add-form');
 var items = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // ----------------------------------------------------
 
@@ -10,7 +11,8 @@ var items = document.getElementById('items');
 form.addEventListener('submit', addItem);
 //remove item
 items.addEventListener('click',deleteItem);
-
+//filter items
+filter.addEventListener('keypress',filterItem );
 // -----------------------------------------------------------
 
 
@@ -55,4 +57,24 @@ if(e.target.classList.contains('delete')){
           //ie the list 
        }
     }
+ }
+
+ //FilterItem
+ function filterItem(e){
+     //convert text to lowercase
+   var text =  e.target.value.toLowerCase();
+   //Get each item
+   var item = items.getElementsByTagName('li');
+   //Convert to array
+   Array.from(item).forEach(function(aItem){
+        var itemName = aItem.firstChild.textContent;
+        //there are two child element of aItem(li): item name and button name
+        if(itemName.toLocaleLowerCase().indexOf(text) != -1){ //indexof returns the position of occurence of specfied value in string
+            aItem.style.display = 'block';                    // for example: e in items lie in 3rd positon
+        }                                                     //returns -1 if not found, eg index of a in items return -1
+        else{
+            aItem.style.display = 'none';
+        }
+   }
+   )
  }
